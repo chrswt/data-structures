1c28344684f88d30d7bc66af5b959a092dad51dd
chrswt <chriswt91@gmail.com>
2016-09-15 16:08:53 -0700

    Implement hash table using pseudoclassical pattern
    Methods:
    - [x] insert
    - [x] remove
    - [x] retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,26 +1,30 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._movedIndexes = {};
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) !== undefined) { // collision detected
+    while (this._storage.get(index) !== undefined) {
+      index++;
+    }
+    this._movedIndexes[k] = index;
+  }
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this._movedIndexes[k] || getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this._movedIndexes[k] || getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
