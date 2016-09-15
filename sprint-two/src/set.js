e1f4f24b38174f8186cb80639a737c6c201a8c97
chrswt <chriswt91@gmail.com>
2016-09-15 10:45:49 -0700

    Implement set with prototypal pattern
    Methods:
    - [x] add
    - [x] contains
    - [x] remove

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return _.indexOf(this._storage, item) >= 0;
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    var location = _.indexOf(this._storage, item);
+
+    this._storage = this._storage.slice(0, location).concat(this._storage.slice(location + 1));
+  }
 };
 
 /*
