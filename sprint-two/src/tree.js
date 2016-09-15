3c14d0fc5e15a258b3279d1f6a860f8f14c6045d
chrswt <chriswt91@gmail.com>
2016-09-15 09:36:12 -0700

    Implement tree with functional-shared pattern
    Methods available:
    - [x] addChild
    - [x] contains

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,8 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,12 +11,18 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else {
+    return _.reduce(this.children, function(foundTarget, child) {
+      return foundTarget = foundTarget ? foundTarget : child.contains(target);
+    }, false);
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
