c8d772cec53004c0e0ae959e8cb674036cd44795
chrswt <chriswt91@gmail.com>
2016-09-16 15:32:21 -0700

    Re-implement set using a hash table

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,28 +1,50 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = [];
+  set._storage = new HashTable();
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  if (!this.contains(item)) {
-    this._storage.push(item);
-  }
+  this._storage.insert(item, item);
 };
 
 setPrototype.contains = function(item) {
-  return _.indexOf(this._storage, item) >= 0;
+  return this._storage.retrieve(item) !== undefined;
+
 };
 
 setPrototype.remove = function(item) {
-  if (this.contains(item)) {
-    var location = _.indexOf(this._storage, item);
-
-    this._storage = this._storage.slice(0, location).concat(this._storage.slice(location + 1));
-  }
+  this._storage.remove(item);
 };
+
+// **Set implemented using arrays**
+// var Set = function() {
+//   var set = Object.create(setPrototype);
+//   set._storage = [];
+//   return set;
+// };
+
+// var setPrototype = {};
+
+// setPrototype.add = function(item) {
+//   if (!this.contains(item)) {
+//     this._storage.push(item);
+//   }
+// };
+
+// setPrototype.contains = function(item) {
+//   return _.indexOf(this._storage, item) >= 0;
+// };
+
+// setPrototype.remove = function(item) {
+//   if (this.contains(item)) {
+//     var location = _.indexOf(this._storage, item);
+
+//     this._storage = this._storage.slice(0, location).concat(this._storage.slice(location + 1));
+//   }
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
