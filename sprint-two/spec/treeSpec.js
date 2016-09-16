6543c2e648d8ece380ee4c5a511f1db56c9ae828
chrswt <chriswt91@gmail.com>
2016-09-15 19:23:06 -0700

    Solve complexity and implement new tests for tree

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -41,4 +41,12 @@ describe('tree', function() {
     expect(tree.contains(8)).to.equal(true);
   });
 
+  it('should support deep nesting', function() {
+    tree.addChild(1);
+    tree.children[0].addChild(2);
+    tree.children[0].children[0].addChild(3);
+    tree.children[0].children[0].children[0].addChild(4);
+    expect(tree.contains(4)).to.equal(true);
+  });
+
 });
